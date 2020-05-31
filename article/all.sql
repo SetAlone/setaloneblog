a994a64c6ab0b3515dee6d7a35f418b783f68baa
SetAlone <51635616+SetAlone@users.noreply.github.com>
2020-05-31 14:11:23 +0800

    mysql中长文本的数据类型

--- article/all.sql
+++ article/all.sql
@@ -1,42 +1,61 @@
 # 所有sql语句
 
-# 管理员表
-drop table if exists 'admin';
+# 管理员表 admin
 CREATE TABLE `admin`
 (
-    `id`       varchar(31) NOT NULL COMMENT '管理员id',
-    `username` varchar(31) NOT NULL COMMENT '账号',
-    `password` varchar(32) NOT NULL COMMENT '密码',
-    PRIMARY KEY (`id`) USING BTREE
+    `admin_id`       bigint(20)  NOT NULL COMMENT '管理员id',
+    `admin_username` varchar(31) NOT NULL COMMENT '账号',
+    `admin_password` varchar(31) NOT NULL COMMENT '密码',
+    PRIMARY KEY (`admin_id`) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
     COMMENT ='管理员表';
 
 # 用户表
-drop table if exists 'user';
 CREATE TABLE `user`
 (
-    `user_id`      varchar(31) NOT NULL COMMENT '用户id',
-    `username`     varchar(31) NOT NULL COMMENT '用户名',
-    `password`     varchar(32) NOT NULL COMMENT '密码',
-    `name`         varchar(31) NOT NULL COMMENT '姓名',
-    `sex`          int(1)               DEFAULT NULL COMMENT '性别。1男2女',
-    `header`       varchar(255)         DEFAULT NULL COMMENT '头像',
-    `nickname`     varchar(31)          DEFAULT '雨落弦鸣' COMMENT '昵称',
-    `user_email`   varchar(31)          DEFAULT NULL COMMENT '邮箱',
-    `gmt_create`   datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `gmt_modified` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `version`      int(11)     NOT NULL DEFAULT '1' COMMENT '乐观锁',
-    `is_deleted`   tinyint(1)  NOT NULL DEFAULT '0' COMMENT '是否删除。0否1是',
+    `user_id`       bigint(20)  NOT NULL COMMENT '用户id',
+    'user_ip'       varchar(30) not null comment '用户ip',
+    `user_name`     varchar(31) NOT NULL COMMENT '用户名',
+    `user_password` varchar(32) NOT NULL COMMENT '密码',
+    'user_phone'    varchar(20) not null comment '用户手机号码',
+    `user_sex`      int(1)               DEFAULT NULL COMMENT '性别。1男2女',
+    `user_header`   varchar(255)         DEFAULT NULL COMMENT '头像',
+    `user_nickname` varchar(31) COMMENT '昵称',
+    `user_email`    varchar(31)          DEFAULT NULL COMMENT '邮箱',
+    `gmt_create`    datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`  datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `version`       int(11)     NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `is_deleted`    tinyint(1)  NOT NULL DEFAULT '0' COMMENT '是否删除。0否1是',
     PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
     COMMENT ='用户表';
 
+# 博文表
+CREATE TABLE `blog`
+(
+    `blog_id`         varchar(31) NOT NULL COMMENT '博客id',
+    `blog_title`      varchar(63) NOT NULL COMMENT '博客id标题',
+    `blog_image`      varchar(255)         DEFAULT NULL COMMENT '封面',
+    `blog_content`    mediumtext  NOT NULL COMMENT '帖子内容',
+    `blog_goods`      int(11)     NOT NULL DEFAULT '0' COMMENT '点赞数',
+    `blog_read`       int(11)     NOT NULL DEFAULT '0' COMMENT '阅读数',
+    `blog_collection` int(11)     NOT NULL DEFAULT '0' COMMENT '收藏数',
+    `blog_type`       int(11)     NOT NULL COMMENT '博客分类',
+    `blog_remark`     varchar(127)         DEFAULT NULL COMMENT '简介',
+    `blog_comment`    int(11)     NOT NULL DEFAULT '0' COMMENT '评论数',
+    `blog_source`     varchar(127)         DEFAULT NULL COMMENT '文章来源（爬虫时使用）',
+    `gmt_create`      datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`    datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `version`         int(11)     NOT NULL DEFAULT '1' COMMENT '乐观锁',
+    `is_deleted`      tinyint(1)  NOT NULL DEFAULT '0' COMMENT '是否删除，0否1是',
+    PRIMARY KEY (`blog_id`) USING BTREE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  ROW_FORMAT = DYNAMIC COMMENT ='博客表'
 
-insert into `user`(`user_id`, `username`, `password`, `name`, `sex`, `header`, `nickname`, `user_email`, `gmt_create`,
-                   `gmt_modified`, `version`, `is_deleted`)
-values ('1', 'setalone', '123456', '蔡伟文', 1, NULL, 'setalone', '2464447440@qq.com', '2020-05-28 08:24:36',
-        '2020-05-29 08:24:42', 1, 0);
+
+
 
 
